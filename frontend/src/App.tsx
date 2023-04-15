2f74d6e25f69d8c8cc59d482b42878507d1701bd
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-15 17:43:46 +0900

    feat: 로그인 요청 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+
+const client_id = 'CLIENT_ID';
+const client_secret = 'CLIENT_SECRET';
+const redirect_uri = 'REDIRECT_URI';
 
 function App() {
-  return <div className="App"></div>;
+  const handleLogin = () => {
+    // 사용자를 인증 서버로 리다이렉션
+    const url = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
+    window.location.href = url;
+  };
+
+  return (
+    <div>
+      <h1>6uidpong</h1>
+      <button onClick={handleLogin}>42 Login</button>
+    </div>
+  );
 }
 
 export default App;

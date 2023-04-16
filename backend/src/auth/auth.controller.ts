c75446a5b2371b623c4577ae8d2ec0a8db20b3f7
zerone015 <zerone015@naver.com>
2023-04-16 09:14:05 +0900

    temp

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { FortyTwoAuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  @UseGuards(FortyTwoAuthGuard)
+  @Get('/login/forty-two')
+  async loginFortyTwo() {
+    return 'success';
+  }
+}

ae2bd0490410b934ffa320a3bd77ca63c9ceeef7
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-17 19:59:47 +0900

    chore: 마이 페이지 배경 이미지 추가

--- frontend/src/MyPage.tsx
+++ frontend/src/MyPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Settings from './Settings';
 
 interface Profile {
   nickname: string;
@@ -19,8 +20,11 @@ interface MyPageProps {
 
 const MyPage: React.FC<MyPageProps> = ({ profile, stats }) => {
   return (
-    <div className="flex flex-col items-center bg-black p-4 font-press-start-2p text-white">
-      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+    <div
+      className="flex h-screen w-screen flex-col items-center bg-cover bg-bottom bg-no-repeat p-4 font-press-start-2p text-white"
+      style={{ backgroundImage: `url(${Settings.LOGIN_BACKGROUND})` }}
+    >
+      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white bg-black p-4">
         <h2 className="mt-2 text-2xl font-bold">{profile.nickname}</h2>
         <img
           className="rounded-full p-7"
@@ -31,13 +35,13 @@ const MyPage: React.FC<MyPageProps> = ({ profile, stats }) => {
       <button className="mb-4 w-full max-w-md rounded border border-white bg-black p-2.5 text-white text-white hover:bg-white hover:text-black">
         Change Profile
       </button>
-      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white bg-black p-4">
         <h3 className="mb-2 text-xl font-bold">Stats</h3>
         <p className="mb-1.5">Wins: {stats.wins}</p>
         <p className="mb-1.5">Losses: {stats.losses}</p>
         <p>Ladder Score: {stats.ladderScore}</p>
       </div>
-      <div className="flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+      <div className="flex w-full max-w-md flex-col items-center rounded border border-white bg-black p-4">
         <h3 className="mb-3 text-xl font-bold">Recent History</h3>
         <ul>
           {stats.recentHistory.map((history, index) => (

39ab6376b988f0cf920b87adb8c8562e70282fbc
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-17 19:21:51 +0900

    feat: 마이 페이지 생성

---
+++ frontend/src/MyPage.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+interface Profile {
+  nickname: string;
+  picture: string;
+}
+
+interface Stats {
+  wins: number;
+  losses: number;
+  ladderScore: number;
+  recentHistory: string[];
+}
+
+interface MyPageProps {
+  profile: Profile;
+  stats: Stats;
+}
+
+const MyPage: React.FC<MyPageProps> = ({ profile, stats }) => {
+  return (
+    <div className="flex flex-col items-center bg-black p-4 font-press-start-2p text-white">
+      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+        <h2 className="mb-2 text-2xl font-bold">Profile</h2>
+        <p className="mb-2">Nickname: {profile.nickname}</p>
+        <img
+          className="rounded-full"
+          src={profile.picture}
+          alt="Profile Picture"
+        />
+      </div>
+      <button className="mb-4 w-full max-w-md rounded border border-white bg-black p-2.5 text-white text-white hover:bg-white hover:text-black">
+        Change Profile
+      </button>
+      <div className="mb-4 flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+        <h3 className="mb-2 text-xl font-bold">Stats</h3>
+        <p className="mb-2">Wins: {stats.wins}</p>
+        <p className="mb-2">Losses: {stats.losses}</p>
+        <p>Ladder Score: {stats.ladderScore}</p>
+      </div>
+      <div className="flex w-full max-w-md flex-col items-center rounded border border-white p-4">
+        <h3 className="mb-2 text-xl font-bold">Recent History</h3>
+        <ul>
+          {stats.recentHistory.map((history, index) => (
+            <li key={index} className="mb-1">
+              {history}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;

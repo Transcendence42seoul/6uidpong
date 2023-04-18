ec5b0b7ec72d230caaf849f276da8ba3f40ef6a2
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-18 16:32:24 +0900

    refactor: 이미지 소스 url 상수화

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ContentBox from '../components/box/ContentBox';
 import HoverButton from '../components/button/HoverButton';
+import Image from '../constants/Image';
 
 const Main: React.FC = () => {
   const navigate = useNavigate();
@@ -16,21 +17,13 @@ const Main: React.FC = () => {
       </HoverButton>
       <div className="group mx-10 w-1/3">
         <ContentBox className="border-8 p-7 group-hover:scale-105 group-hover:border-pink-300 group-hover:bg-pink-300">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/746/746020.png"
-            alt="GAME"
-            onClick={() => navigate('/game')}
-          />
+          <img src={Image.GAME} alt="GAME" onClick={() => navigate('/game')} />
           <h1 className="mt-7 text-4xl group-hover:text-black">GAME</h1>
         </ContentBox>
       </div>
       <div className="group mx-10 w-1/3">
         <ContentBox className="border-8 p-7 group-hover:scale-105 group-hover:border-pink-300 group-hover:bg-pink-300">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/465/465227.png"
-            alt="CHAT"
-            onClick={() => navigate('/chat')}
-          />
+          <img src={Image.CHAT} alt="CHAT" onClick={() => navigate('/chat')} />
           <h1 className="mt-7 text-4xl group-hover:text-black">CHAT</h1>
         </ContentBox>
       </div>

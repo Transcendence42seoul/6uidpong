b79bc58c6ed887f8c5eb3ad0d4e9cad658ea6fdb
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-18 13:16:24 +0900

    chore: 불필요한 코드 제거

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -10,7 +10,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
       <h1 className="mt-0 pt-14 text-center text-3xl text-white">6u!dpong</h1>
       <div className="flex items-center justify-center">
         <button
-          className="text-l mt-5 cursor-pointer border-2 border-white bg-black px-5 py-2.5 text-white hover:bg-white hover:text-black"
+          className="mt-5 cursor-pointer border-2 border-white bg-black px-5 py-2.5 text-white hover:bg-white hover:text-black"
           onClick={onLogin}
         >
           42 LOGIN

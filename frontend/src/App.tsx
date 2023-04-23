520d58056276c56d8bfa2bfe877e56b7438e56fb
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-23 17:32:06 +0900

    feat: 인증 코드로부터 액세스 토큰을 가져오는 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import MyPage from './pages/MyPage';
+
+interface AccessToken {
+  id: string;
+  nickname: string;
+  isTwoFactor: string;
+}
 
 const App: React.FC = () => {
   const stats = {
@@ -13,23 +20,30 @@ const App: React.FC = () => {
     ladderScore: 4242,
     recentHistory: ['Win', 'Loss', 'Win', 'Win', 'Loss'],
   };
+  const [accessToken, setAccessToken] = useState<AccessToken | null>(null);
 
   useEffect(() => {
-    async function sendAuthCode() {
+    const fetchToken = async () => {
       const url = new URL(window.location.href);
       const code = url.searchParams.get('code');
-      await axios.get('/api/v1/auth/social/callback/forty-two', {
-        params: {
-          code,
+      const response = await axios.get(
+        '/api/v1/auth/social/callback/forty-two',
+        {
+          params: {
+            code,
+          },
         },
-      });
-    }
+      );
+      const { accessToken: jwt } = response.data;
+      const payload = jwt_decode<AccessToken>(jwt);
+      setAccessToken(payload);
+    };
 
     if (
       window.location.href ===
       'https://localhost/auth/social/callback/forty-two'
     ) {
-      sendAuthCode();
+      fetchToken();
     }
   }, [window.location.href]);
 

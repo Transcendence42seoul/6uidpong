68b7e11979d315bf8c4fcacfc538dfe4ef55cda0
zerone015 <zerone015@naver.com>
2023-05-05 14:03:59 +0900

    feat: 백엔드 AuthController 단위 테스트 코드 작성 완료

--- srcs/requirements/backend/nest/src/auth/controller/auth.controller.spec.ts
+++ srcs/requirements/backend/nest/src/auth/controller/auth.controller.spec.ts
@@ -5,7 +5,7 @@ import { AuthService } from "../service/auth.service";
 import { AuthController } from "./auth.controller";
 import { FtGuard } from "../guard/ft.guard";
 import { JwtRefreshGuard } from "../guard/jwt-refresh.guard";
-import { HttpStatus } from "@nestjs/common";
+import { HttpStatus, UnauthorizedException } from "@nestjs/common";
 
 jest.mock("../guard/jwt-refresh.guard");
 jest.mock("../guard/ft.guard");
@@ -29,6 +29,7 @@ describe("AuthController", () => {
             generateRefreshToken: jest
               .fn()
               .mockResolvedValue("test_refresh_token"),
+            validateCode: jest.fn(),
           },
         },
         {
@@ -53,11 +54,30 @@ describe("AuthController", () => {
     authService = moduleRef.get<AuthService>(AuthService);
     userService = moduleRef.get<UserService>(UserService);
   });
+  describe("redirectFortytwo", () => {
+    it("should redirect to 42 login page", () => {
+      // Arrange
+      const res: any = {
+        status: jest.fn().mockReturnThis(),
+        redirect: jest.fn(),
+      };
+
+      // Act
+      authController.redirectFortytwo(res);
+
+      // Assert
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.FOUND);
+      expect(res.redirect).toHaveBeenCalledWith(
+        `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.OAUTH_42_CLIENT_ID}&redirect_uri=https://${process.env.HOST_NAME}/auth/social/callback/forty-two&response_type=code&scope=public`
+      );
+    });
+  });
   describe("callbackFortytwo", () => {
     it("should return is2FA: true, id: user.id, accessToken: null", async () => {
+      // Arrange
       const user = new UserEntity();
       user.id = 110731;
-      user.email = "none";
+      user.email = "mock";
       user.is2FA = true;
 
       jest.spyOn(userService, "findUserById").mockResolvedValue(user);
@@ -67,26 +87,36 @@ describe("AuthController", () => {
         status: jest.fn().mockReturnThis(),
       };
 
+      // Act
       const result = await authController.callbackFortytwo(req, res);
-      expect(result).toEqual({ is2FA: true, id: user.id, accessToken: null });
+
+      // Assert
+      expect(result).toStrictEqual({
+        is2FA: true,
+        id: user.id,
+        accessToken: null,
+      });
       expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
     });
     it("should return is2FA: false, id: user.id, accessToken: accessToken", async () => {
+      // Arrange
       const user = new UserEntity();
       user.id = 110731;
-      user.email = "none";
       user.is2FA = false;
 
       jest.spyOn(userService, "findUserById").mockResolvedValue(user);
 
-      const req = { user: { id: user.id } };
+      const req = { user: { id: 110731 } };
       const res: any = {
         cookie: jest.fn(),
         status: jest.fn().mockReturnThis(),
       };
 
+      // Act
       const result = await authController.callbackFortytwo(req, res);
-      expect(result).toEqual({
+
+      // Assert
+      expect(result).toStrictEqual({
         is2FA: false,
         id: user.id,
         accessToken: "test_access_token",
@@ -101,22 +131,25 @@ describe("AuthController", () => {
       });
     });
     it("should create a new user if the user does not exist", async () => {
+      // Arrange
       const user = new UserEntity();
       user.id = 110731;
-      user.email = "none";
-      user.is2FA = false;
+
       jest.spyOn(userService, "createUser").mockResolvedValue(user);
       jest.spyOn(userService, "findUserById").mockResolvedValue(null);
 
-      const req = { user: { id: user.id } };
+      const req = { user: { id: 110731 } };
       const res: any = {
         status: jest.fn().mockReturnThis(),
         setHeader: jest.fn(),
         cookie: jest.fn(),
       };
 
+      // Act
       const result = await authController.callbackFortytwo(req, res);
-      expect(result).toEqual({
+
+      // Assert
+      expect(result).toStrictEqual({
         is2FA: false,
         id: user.id,
         accessToken: "test_access_token",
@@ -133,6 +166,64 @@ describe("AuthController", () => {
         sameSite: "strict",
         path: "/api/v1/auth/token/refresh",
       });
+    });
+  });
+  describe("TwoFactorAuthentication", () => {
+    it("should throw an UnauthorizedException if the code is invalid", async () => {
+      // Arrange
+      const twoFactorAuthDto = {
+        id: 110731,
+        code: "123456",
+      };
+      const res: any = {};
+
+      jest.spyOn(authService, "validateCode").mockResolvedValue(false);
+
+      // Assert
+      await expect(
+        authController.TwoFactorAuthentication(twoFactorAuthDto, res)
+      ).rejects.toThrow(UnauthorizedException);
+    });
+    it("should return an access token if the 2FA code is valid", async () => {
+      // Arrange
+      const twoFactorAuthDto = {
+        id: 110731,
+        code: "123456",
+      };
+      const res: any = {
+        cookie: jest.fn(),
+      };
+
+      jest.spyOn(authService, "validateCode").mockResolvedValue(true);
+
+      // Act
+      const result = await authController.TwoFactorAuthentication(
+        twoFactorAuthDto,
+        res
+      );
+
+      // Assert
+      expect(result).toStrictEqual({ accessToken: "test_access_token" });
+      expect(res.cookie).toHaveBeenCalledWith("refresh", "test_refresh_token", {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        secure: true,
+        sameSite: "strict",
+        path: "/api/v1/auth/token/refresh",
+      });
+    });
+  });
+  describe("refreshToken", () => {
+    it("should return access token", async () => {
+      // Arrange
+      const req = { user: { id: 110731 } };
+
+      // Act
+      const result = await authController.refreshToken(req);
+
+      // Assert
+      expect(result).toStrictEqual({ accessToken: "test_access_token" });
+      expect(authService.generateAccessToken).toHaveBeenCalledWith(req.user.id);
     });
   });
 });

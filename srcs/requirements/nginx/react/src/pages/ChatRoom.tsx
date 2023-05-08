bce3a0b5befb8e84062f9d6acca3fc76fc6357cd
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-08 10:12:55 +0900

    feat: 스크롤 위치 조정

--- srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
@@ -1,4 +1,10 @@
-import React, { FormEvent, useCallback, useEffect, useState } from 'react';
+import React, {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { io } from 'socket.io-client';
 
 interface Chat {
@@ -11,13 +17,23 @@ const socket = io('/chat');
 const ChatRoom: React.FC = () => {
   const [chats, setChats] = useState<Chat[]>([]);
   const [message, setMessage] = useState<string>('');
+  const chatContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current } = chatContainer;
+    if (!current) return;
+
+    const { clientHeight, scrollHeight } = current;
+    if (scrollHeight > clientHeight) {
+      current.scrollTop = scrollHeight - clientHeight;
+    }
+  }, [chats.length]);
 
   useEffect(() => {
     const messageHandler = (chat: Chat) =>
       setChats((prevChats) => [...prevChats, chat]);
 
     socket.on('message', messageHandler);
-
     return () => {
       socket.off('message', messageHandler);
     };

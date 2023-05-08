922b2f2757267c7bf7ed8a7b08595d33969cdb46
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-08 10:01:47 +0900

    feat: 메시지 전송 이벤트 추가

--- srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useCallback, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 interface Chat {
@@ -10,6 +10,7 @@ const socket = io('/chat');
 
 const ChatRoom: React.FC = () => {
   const [chats, setChats] = useState<Chat[]>([]);
+  const [message, setMessage] = useState<string>('');
 
   useEffect(() => {
     const messageHandler = (chat: Chat) =>
@@ -21,4 +22,19 @@ const ChatRoom: React.FC = () => {
       socket.off('message', messageHandler);
     };
   }, []);
+
+  const onSendMessage = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!message) {
+        return alert('메시지를 입력해 주세요.');
+      }
+
+      socket.emit('message', message, (chat: Chat) => {
+        setChats((prevChats) => [...prevChats, chat]);
+        setMessage('');
+      });
+    },
+    [message],
+  );
 };

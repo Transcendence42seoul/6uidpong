e6d60a2564adba1326b52c6d169598711e286e99
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-08 10:00:22 +0900

    feat: 메시지 전송 이벤트 리스너 추가

--- srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 interface Chat {
@@ -10,4 +10,15 @@ const socket = io('/chat');
 
 const ChatRoom: React.FC = () => {
   const [chats, setChats] = useState<Chat[]>([]);
+
+  useEffect(() => {
+    const messageHandler = (chat: Chat) =>
+      setChats((prevChats) => [...prevChats, chat]);
+
+    socket.on('message', messageHandler);
+
+    return () => {
+      socket.off('message', messageHandler);
+    };
+  }, []);
 };

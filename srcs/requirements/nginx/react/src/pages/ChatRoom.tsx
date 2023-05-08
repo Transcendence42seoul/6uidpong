8f31ab60bb315dbfbac4df6d57a5fcf4f2e56f14
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-08 09:45:28 +0900

    feat: 채팅 인터페이스 추가

--- srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoom.tsx
@@ -1,3 +1,13 @@
+import React, { useState } from 'react';
 import { io } from 'socket.io-client';
 
+interface Chat {
+  username: string;
+  message: string;
+}
+
 const socket = io('/chat');
+
+const ChatRoom: React.FC = () => {
+  const [chats, setChats] = useState<Chat[]>([]);
+};

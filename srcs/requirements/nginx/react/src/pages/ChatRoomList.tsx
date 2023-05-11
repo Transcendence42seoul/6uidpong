10edc76bc74e21a1a6c0083e61cae854c67ad52b
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-11 20:16:09 +0900

    fix: 채팅방 인터페이스 속성 타입 수정
    변경 사항:
    - roomId: number -> string
    - lastMessageTime: Date -> string

--- srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
@@ -7,9 +7,9 @@ interface ChatRoomListProps {
 }
 
 interface Room {
-  roomId: number;
+  roomId: string;
   lastMessage: string;
-  lastMessageTime: Date;
+  lastMessageTime: string;
   interlocutor: string;
   interlocutorImage: string;
 }
@@ -43,14 +43,13 @@ const ChatRoomList: React.FC<ChatRoomListProps> = ({ socket }) => {
       <h1 className="mb-4 text-3xl font-bold">Chat</h1>
       <ul className="w-full max-w-3xl">
         {rooms.map((room) => {
-          const formattedTime = room.lastMessageTime.toLocaleTimeString(
-            'ko-KR',
-            {
-              hour: 'numeric',
-              minute: 'numeric',
-              hour12: true,
-            },
-          );
+          const formattedTime = new Date(
+            room.lastMessageTime,
+          ).toLocaleTimeString('ko-KR', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          });
           return (
             <li
               key={room.roomId}

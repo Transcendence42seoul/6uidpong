e7ac8db486bca4fdb976359bb69a5d0464a9c8bc
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-11 09:57:57 +0900

    feat: 메인 페이지 CHAT 버튼 호버 이펙트 추가
    내부에 DM 및 CHANNEL 버튼이 나타남

--- srcs/requirements/nginx/react/src/pages/Main.tsx
+++ srcs/requirements/nginx/react/src/pages/Main.tsx
@@ -21,12 +21,26 @@ const Main: React.FC = () => {
           <h1 className="mt-7 text-4xl group-hover:text-black">GAME</h1>
         </ContentBox>
       </button>
-      <button className="group mx-10 w-1/3" onClick={() => navigate('/chat')}>
-        <ContentBox className="border-8 p-7 group-hover:scale-105 group-hover:border-pink-300 group-hover:bg-pink-300">
+      <div className="group relative mx-10 w-1/3">
+        <ContentBox className="border-8 p-7 group-hover:scale-105 group-hover:border-pink-300 group-hover:bg-pink-300 group-hover:opacity-70">
           <img src={Image.CHAT} alt="CHAT" />
           <h1 className="mt-7 text-4xl group-hover:text-black">CHAT</h1>
         </ContentBox>
-      </button>
+        <div className="absolute inset-0 hidden flex-col justify-center group-hover:flex">
+          <HoverButton
+            onClick={() => navigate('/chat')}
+            className="mb-10 h-1/4 w-full text-lg"
+          >
+            DM
+          </HoverButton>
+          <HoverButton
+            onClick={() => navigate('/chat')}
+            className="h-1/4 w-full text-lg"
+          >
+            CHANNEL
+          </HoverButton>
+        </div>
+      </div>
     </div>
   );
 };

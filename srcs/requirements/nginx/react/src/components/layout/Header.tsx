54ce435e0b1390c2ec61742a7b1c98d03b8896bd
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-12 19:42:05 +0900

    feat: 헤더 컴포넌트의 메인 페이지 이동 버튼 위치 조정
    좌측 상단 -> 우측 상단

--- srcs/requirements/nginx/react/src/components/layout/Header.tsx
+++ srcs/requirements/nginx/react/src/components/layout/Header.tsx
@@ -6,20 +6,20 @@ const Header: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <>
+    <div className="absolute right-0 top-0 m-4 flex space-x-4">
       <HoverButton
         onClick={() => navigate('/')}
-        className="absolute left-0 top-0 m-4 rounded border-2 p-2.5"
+        className="rounded border-2 p-2.5"
       >
         Home
       </HoverButton>
       <HoverButton
         onClick={() => navigate('/my-page')}
-        className="absolute right-0 top-0 m-4 rounded border-2 p-2.5"
+        className="rounded border-2 p-2.5"
       >
         My Page
       </HoverButton>
-    </>
+    </div>
   );
 };
 

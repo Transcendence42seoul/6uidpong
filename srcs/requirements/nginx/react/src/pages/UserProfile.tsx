ae0e98cdfb36e4963cff3af63996a146a9f9c7f2
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-12 18:27:39 +0900

    feat: 유저 프로필 페이지 css 변경

--- srcs/requirements/nginx/react/src/pages/UserProfile.tsx
+++ srcs/requirements/nginx/react/src/pages/UserProfile.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useCallAPI from '../api';
+import CircularImage from '../components/container/CircularImage';
+import ContentBox from '../components/container/ContentBox';
 
 export interface User {
   nickname: string;
@@ -24,27 +26,31 @@ const UserProfile: React.FC = () => {
   }, []);
 
   return (
-    <div className="font-pixel flex flex-col items-center">
-      <img className="h-32 w-32 rounded-full" src={user?.image} alt="Profile" />
-      <h2 className="mt-4 text-lg font-semibold">
-        {user?.nickname ?? 'Loading...'}
-      </h2>
-      <div className="mt-2 flex">
-        <p className="mr-4 text-sm">Wins: {user?.winStat}</p>
-        <p className="text-sm">Losses: {user?.loseStat}</p>
-      </div>
-      <p className="mt-2 text-sm">Ladder Score: {user?.ladderScore}</p>
-      <div className="mt-4 flex">
-        <button className="mr-2 rounded bg-blue-500 px-4 py-2 text-white">
-          Game
-        </button>
-        <button className="mr-2 rounded bg-green-500 px-4 py-2 text-white">
-          DM
-        </button>
-        <button className="rounded bg-red-500 px-4 py-2 text-white">
-          Block
-        </button>
-      </div>
+    <div className="flex flex-col items-center p-20">
+      <ContentBox className="p-4">
+        <h2 className="text-lg font-semibold">
+          {user?.nickname ?? 'Loading...'}
+        </h2>
+        <CircularImage
+          src={user?.image}
+          alt="Profile"
+          className="m-2.5 h-32 w-32"
+        />
+        <p className="mt-1 text-sm">Wins: {user?.winStat}</p>
+        <p className="mt-1 text-sm">Losses: {user?.loseStat}</p>
+        <p className="mt-1 text-sm">Ladder Score: {user?.ladderScore}</p>
+        <div className="mt-4 flex">
+          <button className="mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-400">
+            Game
+          </button>
+          <button className="mr-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-400">
+            DM
+          </button>
+          <button className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-400">
+            Block
+          </button>
+        </div>
+      </ContentBox>
     </div>
   );
 };

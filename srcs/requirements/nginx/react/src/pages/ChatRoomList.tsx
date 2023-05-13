2df05b6b6f32a8f6eb0cf8da0965db753c84d258
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-13 20:12:11 +0900

    fix: 98bbcd56 롤백

--- srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
@@ -17,53 +17,6 @@ interface Room {
   interlocutorImage: string;
   hasNewMsg: string;
 }
-interface RoomProps {
-  room: Room;
-  navigate: ReturnType<typeof useNavigate>;
-}
-
-const RoomItem = React.memo<RoomProps>(({ room, navigate }) => {
-  const formattedTime = new Date(room.lastMessageTime).toLocaleTimeString(
-    'ko-KR',
-    {
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true,
-    },
-  );
-
-  return (
-    <li
-      key={room.roomId}
-      className="flex items-center justify-between border-b border-gray-200 bg-white px-4 py-2"
-      onDoubleClick={() =>
-        navigate(`/chat/${room.roomId}`, {
-          state: { interlocutorId: room.interlocutorId },
-        })
-      }
-    >
-      <div className="flex items-center">
-        <CircularImage
-          src={room.interlocutorImage}
-          alt="Interlocutor"
-          className="mr-2 h-10 w-10"
-        />
-        <div>
-          <span>{room.interlocutor}</span>
-          <p className="text-sm text-gray-600">{room.lastMessage}</p>
-        </div>
-      </div>
-      <div className="flex items-center">
-        {room.hasNewMsg === 'true' && (
-          <div className="mr-3 h-2 w-2 rounded-full bg-red-500" />
-        )}
-        <span className="text-sm text-gray-600">{formattedTime}</span>
-      </div>
-    </li>
-  );
-});
-
-RoomItem.displayName = 'RoomItem';
 
 const ChatRoomList: React.FC<ChatRoomListProps> = ({ socket }) => {
   const navigate = useNavigate();
@@ -99,9 +52,44 @@ const ChatRoomList: React.FC<ChatRoomListProps> = ({ socket }) => {
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="mb-4 text-3xl font-bold">Chat</h1>
       <ul className="w-full max-w-3xl">
-        {rooms.map((room) => (
-          <RoomItem key={room.roomId} room={room} navigate={navigate} />
-        ))}
+        {rooms.map((room) => {
+          const formattedTime = new Date(
+            room.lastMessageTime,
+          ).toLocaleTimeString('ko-KR', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          });
+          return (
+            <li
+              key={room.roomId}
+              className="flex items-center justify-between border-b border-gray-200 bg-white px-4 py-2"
+              onDoubleClick={() =>
+                navigate(`/chat/${room.roomId}`, {
+                  state: { interlocutorId: room.interlocutorId },
+                })
+              }
+            >
+              <div className="flex items-center">
+                <CircularImage
+                  src={room.interlocutorImage}
+                  alt="Interlocutor"
+                  className="mr-2 h-10 w-10"
+                />
+                <div>
+                  <span>{room.interlocutor}</span>
+                  <p className="text-sm text-gray-600">{room.lastMessage}</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                {room.hasNewMsg === 'true' && (
+                  <div className="mr-3 h-2 w-2 rounded-full bg-red-500" />
+                )}
+                <span className="text-sm text-gray-600">{formattedTime}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );

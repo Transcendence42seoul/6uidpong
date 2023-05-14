a792f9305febb1361fd2bedd94507c18880fa052
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-14 16:11:05 +0900

    feat: 채팅방 목록을 최근 메시지 순으로 정렬

--- srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
+++ srcs/requirements/nginx/react/src/pages/ChatRoomList.tsx
@@ -50,46 +50,52 @@ const ChatRoomList: React.FC<ChatRoomListProps> = ({ socket }) => {
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="mb-4 text-3xl font-bold">Chat</h1>
       <ul className="w-full max-w-3xl">
-        {rooms.map((room) => {
-          const formattedTime = new Date(
-            room.lastMessageTime,
-          ).toLocaleTimeString('ko-KR', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-          });
-          return (
-            <li
-              key={room.roomId}
-              className="flex items-center justify-between border-b border-gray-200 bg-white px-4 py-2"
-              onDoubleClick={() =>
-                navigate(`/chat/${room.roomId}`, {
-                  state: { interlocutorId: room.interlocutorId },
-                })
-              }
-            >
-              <div className="flex items-center">
-                <CircularImage
-                  src={room.interlocutorImage}
-                  alt="Interlocutor"
-                  className="mr-2 h-10 w-10"
-                />
-                <div>
-                  <span>{room.interlocutor}</span>
-                  <p className="text-sm text-gray-600">{room.lastMessage}</p>
-                </div>
-              </div>
-              <div className="flex items-center">
-                {room.newMsgCount > 0 && (
-                  <div className="mr-3 rounded-full bg-red-500 text-white">
-                    {room.newMsgCount}
+        {rooms
+          .sort(
+            (lhs, rhs) =>
+              new Date(rhs.lastMessageTime).getTime() -
+              new Date(lhs.lastMessageTime).getTime(),
+          )
+          .map((room) => {
+            const formattedTime = new Date(
+              room.lastMessageTime,
+            ).toLocaleTimeString('ko-KR', {
+              hour: 'numeric',
+              minute: 'numeric',
+              hour12: true,
+            });
+            return (
+              <li
+                key={room.roomId}
+                className="flex items-center justify-between border-b border-gray-200 bg-white px-4 py-2"
+                onDoubleClick={() =>
+                  navigate(`/chat/${room.roomId}`, {
+                    state: { interlocutorId: room.interlocutorId },
+                  })
+                }
+              >
+                <div className="flex items-center">
+                  <CircularImage
+                    src={room.interlocutorImage}
+                    alt="Interlocutor"
+                    className="mr-2 h-10 w-10"
+                  />
+                  <div>
+                    <span>{room.interlocutor}</span>
+                    <p className="text-sm text-gray-600">{room.lastMessage}</p>
                   </div>
-                )}
-                <span className="text-sm text-gray-600">{formattedTime}</span>
-              </div>
-            </li>
-          );
-        })}
+                </div>
+                <div className="flex items-center">
+                  {room.newMsgCount > 0 && (
+                    <div className="mr-3 rounded-full bg-red-500 text-white">
+                      {room.newMsgCount}
+                    </div>
+                  )}
+                  <span className="text-sm text-gray-600">{formattedTime}</span>
+                </div>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );

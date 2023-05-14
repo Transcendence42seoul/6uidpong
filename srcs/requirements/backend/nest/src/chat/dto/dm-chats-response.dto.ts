b9d0a83eaeb5745fe82df2444dc6c758d167cd73
zerone015 <zerone015@naver.com>
2023-05-14 13:40:09 +0900

    feat: DmChatResponseDto에 chat id 필드 추가

--- srcs/requirements/backend/nest/src/chat/dto/dm-chats-response.dto.ts
+++ srcs/requirements/backend/nest/src/chat/dto/dm-chats-response.dto.ts
@@ -2,12 +2,15 @@ import { DmChatEntity } from "../entity/dm-chat.entity";
 
 export class DmChatResponseDto {
   constructor(dmChatEntity: DmChatEntity) {
+    this.id = dmChatEntity.id;
     this.userId = dmChatEntity.user.id;
     this.nickname = dmChatEntity.user.nickname;
     this.image = dmChatEntity.user.image;
     this.message = dmChatEntity.message;
     this.createdAt = dmChatEntity.createdAt;
   }
+
+  id: number;
 
   userId: number;
 

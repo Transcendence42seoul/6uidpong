79b6981cda2961b518a261ceb40d88ad20f51fd0
jjongs2 <kijsong@student.42seoul.kr>
2023-05-16 17:42:10 +0900

    feat: 친구 목록에 더블 클릭 이벤트 추가
    해당 유저의 프로필로 이동

--- srcs/requirements/nginx/react/src/pages/FriendList.tsx
+++ srcs/requirements/nginx/react/src/pages/FriendList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { User } from './UserProfile';
 import CircularImage from '../components/container/CircularImage';
 
 const FriendList: React.FC = () => {
   const friends: User[] = [
     {
+      id: 110729,
       nickname: 'kijsong',
       image:
         'https://cdn.intra.42.fr/users/a99b98748e81f651c11c5fa2ccbb753e/kijsong.jpg',
@@ -13,6 +15,7 @@ const FriendList: React.FC = () => {
       ladderScore: 1000,
     },
     {
+      id: 110731,
       nickname: 'yoson',
       image:
         'https://cdn.intra.42.fr/users/40840e98c56e893af845a7d2b05e631d/yoson.jpg',
@@ -21,6 +24,7 @@ const FriendList: React.FC = () => {
       ladderScore: 1000,
     },
     {
+      id: 123456,
       nickname: 'wocheon',
       image:
         'https://cdn.intra.42.fr/users/bd9d267e40c02269bbdcd09fe4924419/wocheon.jpg',
@@ -29,14 +33,22 @@ const FriendList: React.FC = () => {
       ladderScore: 1000,
     },
   ];
+
+  const navigate = useNavigate();
+
   return (
     <div className="p-4">
       <h2 className="mb-4 text-2xl font-bold text-gray-100">Friends</h2>
       <ul className="space-y-2">
-        {friends.map(({ nickname, image }) => (
+        {friends.map(({ id, nickname, image }) => (
           <li
             key={nickname}
             className="flex items-center border-2 border-white bg-black p-2"
+            onDoubleClick={() =>
+              navigate(`/profile/${id}`, {
+                state: { nickname },
+              })
+            }
           >
             <CircularImage
               src={image}

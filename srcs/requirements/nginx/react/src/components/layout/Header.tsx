c5182008e07a72d57b6b6e9b342662a251f774f6
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-17 15:23:48 +0900

    chore: 헤더 컴포넌트 내부 변수 이름 변경
    변경 사항:
    - results -> searchResults
    - result -> user

--- srcs/requirements/nginx/react/src/components/layout/Header.tsx
+++ srcs/requirements/nginx/react/src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
   const callAPI = useCallAPI();
   const navigate = useNavigate();
   const [search, setSearch] = useState<string>('');
-  const [results, setResults] = useState<User[]>([]);
+  const [searchResults, setSearchResults] = useState<User[]>([]);
 
   const handleClickHome = () => navigate('/');
   const handleClickMyPage = () => navigate('/my-page');
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
       nickname,
     };
     const data: User[] = await callAPI('/api/v1/users/search', params);
-    setResults(data);
+    setSearchResults(data);
   };
 
   return (
@@ -39,8 +39,8 @@ const Header: React.FC = () => {
         />
         {search && (
           <ul className="absolute z-10 rounded bg-white p-2 shadow-md">
-            {results.map((result) => {
-              const { nickname, image } = result;
+            {searchResults.map((user) => {
+              const { nickname, image } = user;
               return (
                 <li key={nickname} className="border-b border-gray-300 py-1">
                   {nickname}

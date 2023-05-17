41c42332483d00db4745d31864fd68da525998f5
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-17 17:43:32 +0900

    feat: 유저 검색 결과 목록의 표시 여부를 결정하는 상태 추가

--- srcs/requirements/nginx/react/src/components/layout/Header.tsx
+++ srcs/requirements/nginx/react/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useCallAPI from '../../api';
 import { User } from '../../pages/UserProfile';
@@ -9,23 +9,47 @@ import CircularImage from '../container/CircularImage';
 const Header: React.FC = () => {
   const callAPI = useCallAPI();
   const navigate = useNavigate();
+
+  const searchResultsRef = useRef<HTMLUListElement>(null);
   const [search, setSearch] = useState<string>('');
   const [searchResults, setSearchResults] = useState<User[]>([]);
+  const [showSearchResults, setShowSearchResults] = useState<boolean>(false);
 
   const handleClickHome = () => navigate('/');
   const handleClickMyPage = () => navigate('/my-page');
-  const handleClickUser = (id: number) => navigate(`/profile/${id}`);
+  const handleClickUser = (id: number) => {
+    setShowSearchResults(false);
+    navigate(`/profile/${id}`);
+  };
+
+  const handleClickSearch = () => setShowSearchResults(true);
   const handleChangeSearch = async (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     const nickname = event.target.value;
     setSearch(nickname);
+    setShowSearchResults(!!nickname);
     const params = {
       nickname,
     };
     const data: User[] = await callAPI('/api/v1/users/search', params);
     setSearchResults(data);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        showSearchResults &&
+        !searchResultsRef.current?.contains(event.target as Node)
+      ) {
+        setShowSearchResults(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showSearchResults]);
 
   return (
     <div className="m-4 flex items-center justify-between">
@@ -38,9 +62,10 @@ const Header: React.FC = () => {
           placeholder="Search users"
           value={search}
           onChange={handleChangeSearch}
+          onClick={handleClickSearch}
           className="w-full rounded border border-white p-2 shadow"
         />
-        {search && (
+        {showSearchResults && (
           <ul className="absolute z-10 flex w-full flex-col rounded border-2 bg-black bg-white px-2.5 pb-2 pt-1.5 shadow-md">
             {searchResults.map((user) => {
               const { id, nickname, image } = user;

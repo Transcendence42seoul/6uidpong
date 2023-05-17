248a2a8373b2ff8146f2263b1bd721868b1bd3f8
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-17 16:39:40 +0900

    feat: 유저 검색 결과의 요소 타입 변경 및 클릭 이벤트 추가
    li -> button

--- srcs/requirements/nginx/react/src/components/layout/Header.tsx
+++ srcs/requirements/nginx/react/src/components/layout/Header.tsx
@@ -40,18 +40,22 @@ const Header: React.FC = () => {
           className="w-full rounded border border-white p-2 shadow"
         />
         {search && (
-          <ul className="absolute z-10 w-full rounded border-2 bg-black bg-white px-2.5 pb-2 pt-1.5 shadow-md">
+          <ul className="absolute z-10 flex w-full flex-col rounded border-2 bg-black bg-white px-2.5 pb-2 pt-1.5 shadow-md">
             {searchResults.map((user) => {
-              const { nickname, image } = user;
+              const { id, nickname, image } = user;
               return (
-                <li key={nickname} className="border-b border-gray-300 py-1">
+                <button
+                  key={nickname}
+                  className="flex border-b border-gray-300 py-1"
+                  onClick={() => navigate(`/profile/${id}`)}
+                >
                   <CircularImage
                     src={image}
                     alt={nickname}
                     className="mr-2 h-6 w-6 align-bottom"
                   />
                   {nickname}
-                </li>
+                </button>
               );
             })}
           </ul>

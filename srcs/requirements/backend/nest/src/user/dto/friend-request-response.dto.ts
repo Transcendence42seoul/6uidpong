58043540edd688eb89eb1299f8d0015bf8963f8f
zerone015 <zerone015@naver.com>
2023-05-19 17:24:36 +0900

    feat: FriendRequestResponseDto에 image 필드 추가

--- srcs/requirements/backend/nest/src/user/dto/friend-request-response.dto.ts
+++ srcs/requirements/backend/nest/src/user/dto/friend-request-response.dto.ts
@@ -4,8 +4,11 @@ export class FriendRequestResponseDto {
   constructor(entity: FriendRequestEntity) {
     this.id = entity.from.id;
     this.nickname = entity.from.nickname;
+    this.image = entity.from.image;
   }
   id: number;
 
   nickname: string;
+
+  image: string;
 }

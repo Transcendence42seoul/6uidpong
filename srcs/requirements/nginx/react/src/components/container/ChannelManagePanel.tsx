88d771acf1d7b8c0de9c94acdfbd83d0aff9252d
jjongs2 <jjongs@hanyang.ac.kr>
2023-05-26 21:02:30 +0900

    feat: 채널 설정의 관리 패널에 버튼 클릭 이벤트 추가

--- srcs/requirements/nginx/react/src/components/container/ChannelManagePanel.tsx
+++ srcs/requirements/nginx/react/src/components/container/ChannelManagePanel.tsx
@@ -4,36 +4,32 @@ import HoverButton from '../button/HoverButton';
 import ContentBox from './ContentBox';
 
 const ChannelManagePanel: React.FC = () => {
-  const [showAdminModal, setShowAdminModal] = useState<boolean>(false);
   const [showBanModal, setShowBanModal] = useState<boolean>(false);
   const [showKickModal, setShowKickModal] = useState<boolean>(false);
   const [showMuteModal, setShowMuteModal] = useState<boolean>(false);
-  const [showOwnerModal, setShowOwnerModal] = useState<boolean>(false);
+  const [showChangeOwnerModal, setShowChangeOwnerModal] =
+    useState<boolean>(false);
+  const [showManageAdminModal, setShowManageAdminModal] =
+    useState<boolean>(false);
 
-  const handleChangeOwnerClick = () => {};
-  const handleManageAdminClick = () => {};
+  const handleBanClick = () => setShowBanModal(true);
+  const handleKickClick = () => setShowKickModal(true);
+  const handleMuteClick = () => setShowMuteModal(true);
+  const handleChangeOwnerClick = () => setShowChangeOwnerModal(true);
+  const handleManageAdminClick = () => setShowManageAdminModal(true);
 
   return (
     <div className="w-full max-w-sm items-center space-y-4 p-4">
       <ContentBox className="w-full max-w-xs space-y-2 p-4">
         <span className="text-md">Manage Users</span>
         <div className="flex space-x-1">
-          <HoverButton
-            onClick={handleManageAdminClick}
-            className="rounded border p-2"
-          >
+          <HoverButton onClick={handleMuteClick} className="rounded border p-2">
             Mute
           </HoverButton>
-          <HoverButton
-            onClick={handleManageAdminClick}
-            className="rounded border p-2"
-          >
+          <HoverButton onClick={handleKickClick} className="rounded border p-2">
             Kick
           </HoverButton>
-          <HoverButton
-            onClick={handleManageAdminClick}
-            className="rounded border p-2"
-          >
+          <HoverButton onClick={handleBanClick} className="rounded border p-2">
             Ban
           </HoverButton>
         </div>

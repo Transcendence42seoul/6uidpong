b76e49374872dfb4fb71da19aaf1f1c0e6bb059a
jjongs2 <kijsong@student.42seoul.kr>
2023-06-02 13:28:44 +0900

    fix: 채널 비밀번호 입력 모달에서 채널 페이지로 전달하는 변수 형식 수정

--- srcs/requirements/nginx/react/src/components/modal/ChannelPasswordModal.tsx
+++ srcs/requirements/nginx/react/src/components/modal/ChannelPasswordModal.tsx
@@ -24,7 +24,7 @@ const ChannelPasswordModal: React.FC<ChannelPasswordModalProps> = ({
 
   const handleConfirmClick = () => {
     navigate(`/channel/${channelId}`, {
-      state: password,
+      state: { password },
     });
   };
 

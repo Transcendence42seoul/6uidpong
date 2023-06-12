9e801a6b59fcdfc0c10d2146077d52ec287db5ed
zerone015 <zerone015@naver.com>
2023-06-12 16:12:36 +0900

    feat: Dm Join 반환 필드에 isBlocked 추가

--- srcs/requirements/backend/nest/src/chat/dto/dm/join-response.ts
+++ srcs/requirements/backend/nest/src/chat/dto/dm/join-response.ts
@@ -2,14 +2,22 @@ import { DmChat } from "../../entity/dm/dm-chat.entity";
 import { ChatResponse } from "./chat-response";
 
 export class JoinResponse {
-  constructor(roomId: number, newMsgCount: number, entities: DmChat[]) {
+  constructor(
+    roomId: number,
+    newMsgCount: number,
+    isBlocked: boolean,
+    entities: DmChat[]
+  ) {
     this.roomId = roomId;
     this.newMsgCount = newMsgCount;
+    this.isBlocked = isBlocked;
     this.chats = entities.map((entity) => new ChatResponse(entity));
   }
   readonly roomId: number;
 
   readonly newMsgCount: number;
+
+  readonly isBlocked: boolean;
 
   readonly chats: ChatResponse[];
 }

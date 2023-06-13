953da49a943d370f925c3d7a61722cc74a4fe8b6
jjongs2 <jjongs@hanyang.ac.kr>
2023-06-13 14:54:56 +0900

    feat: 닉네임 변경 시 유효성 검사
    영어/숫자 4-14자

--- srcs/requirements/nginx/react/src/components/custom/Nickname.tsx
+++ srcs/requirements/nginx/react/src/components/custom/Nickname.tsx
@@ -1,42 +1,68 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
 
 import selectAuth from '../../features/auth/authSelector';
-import HoverButton from '../button/HoverButton';
+import useCallApi from '../../utils/useCallApi';
 
 const Nickname: React.FC = () => {
-  const { accessToken, tokenInfo } = selectAuth();
+  const callApi = useCallApi();
+
+  const { tokenInfo } = selectAuth();
   const myId = tokenInfo?.id;
 
+  const [isValid, setIsValid] = useState<boolean>(true);
   const [nickname, setNickname] = useState<string>('');
 
-  const handleNickname = () => {
-    axios
-      .put(
-        `api/v1/users/${myId}/nickname`,
-        { nickname },
-        {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        },
-      )
-      .then((response: AxiosResponse<void>) => {
-        alert('닉네임 변경 완료');
-      })
-      .catch((error: AxiosError) => {
-        alert('닉네임 변경 실패');
-      });
-  };
+  const handleNicknameChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setNickname(event.target.value);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (!isValid) return;
+    const config = {
+      url: `/api/v1/users/${myId}/nickname`,
+      method: 'put',
+      data: { nickname },
+    };
+    try {
+      callApi(config);
+    } catch (error) {
+      setIsValid(false);
+    }
+  }, [isValid]);
+
+  useEffect(() => {
+    if (!nickname) return;
+    const timeoutId = setTimeout(() => {
+      const regex = /^[a-zA-Z0-9]{4,14}$/;
+      setIsValid(regex.test(nickname));
+    }, 500);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [nickname]);
+
   return (
-    <>
-      <h1>닉네임 변경</h1>
+    <label htmlFor="nickname" className="max-w-md space-y-1.5">
+      Nickname
       <input
         type="text"
-        name="nickname"
-        onChange={(event) => setNickname(event.target.value)}
-        className="my-4 w-full max-w-md rounded-md border border-gray-400 px-4 py-2 text-gray-900 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-pink-500"
+        id="nickname"
+        value={nickname}
+        onChange={handleNicknameChange}
+        className={`mt-4 w-full rounded px-4 py-2 text-gray-900 focus:outline-none ${
+          isValid ? 'border' : 'border-2 border-red-500'
+        }`}
       />
-      <HoverButton onClick={handleNickname}>전송</HoverButton>
-    </>
+      {!isValid && (
+        <p className="pl-2.5 text-left text-xs text-red-500">
+          Sorry, only letters(English) and numbers are allowed between 4 and 14
+          characters long.
+        </p>
+      )}
+    </label>
   );
 };
 

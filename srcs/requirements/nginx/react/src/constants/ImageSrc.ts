ea73ebd7732c6995d506a8042be00ff44b1870ad
jjongs2 <kijsong@student.42seoul.kr>
2023-06-17 22:33:34 +0900

    chore: 파일 확장자 변경

---
+++ srcs/requirements/nginx/react/src/constants/ImageSrc.ts
@@ -0,0 +1,22 @@
+class ImageSrc {
+  static readonly CHAT =
+    'https://cdn-icons-png.flaticon.com/512/465/465227.png';
+
+  static readonly GAME =
+    'https://cdn-icons-png.flaticon.com/512/746/746020.png';
+
+  static readonly LOADING = 'https://i.imgur.com/dMRGsI1.png';
+
+  static readonly LOCK =
+    'https://static.vecteezy.com/system/resources/previews/012/852/009/large_2x/pixel-art-lock-isolated-free-vector.jpg';
+
+  static readonly MODAL_IMAGE = 'https://i.imgur.com/lfaWNsI.png';
+
+  static readonly MATCH_IMAGE =
+    'https://media.tenor.com/PeRI5dkeLFkAAAAj/tower-defense-simulator-roblox.gif';
+
+  static readonly MATCH_SUCCESS_IMAGE =
+    'https://media.tenor.com/7HTQnebtrOsAAAAj/dbz-pixel.gif';
+}
+
+export default ImageSrc;
